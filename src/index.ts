8e183d2a2fe3b31b8ca5cf575a8b13f37ff9e6df
freitzzz <1160907@isep.ipp.pt>
2024-01-08 19:00:42 +0000

    chore: init scheduled cloudflare worker

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Env, fromEnv } from './config';
+
+export default {
+	async scheduled(
+		controller: ScheduledController,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<void> {
+		const config = fromEnv(env);
+
+		console.info(`Hello ${config.awesomeSecret}!`);
+
+		return Promise.resolve();
+	}
+};
