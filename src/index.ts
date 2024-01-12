e2c97b840aa6905f646896ddc4a09469f87574ad
freitzzz <1160907@isep.ipp.pt>
2024-01-12 13:22:01 +0000

    feat(fetch-stories): proof of concept using https://github.com/dilame/instagram-private-api/blob/master/examples/user-stories-watching.example.ts#L13

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-import sum from './sum';
+import { IgApiClient } from 'instagram-private-api'
+import { config as dotenv } from 'dotenv';
 
-console.log(sum(40, 2));
+type Config = {
+    username: string;
+    password: string;
+}
+
+
+dotenv();
+const config = <Config>{ ...process.env };
+
+
+(async () => {
+    const ig = new IgApiClient();
+
+    ig.state.generateDevice(config.username);
+    const auth = await ig.account.login(config.username, config.password);
+
+    console.log(JSON.stringify(auth));
+
+    const targetUser = await ig.user.searchExact('ronaldo'); // getting exact user by login
+    const reelsFeed = ig.feed.reelsMedia({ // working with reels media feed (stories feed)
+        userIds: [targetUser.pk], // you can specify multiple user id's, "pk" param is user id
+    });
+
+    const storyItems = await reelsFeed.items(); // getting reels, see "account-followers.feed.example.ts" if you want to know how to work with feeds
+
+    console.log(JSON.stringify(storyItems));
+})();
