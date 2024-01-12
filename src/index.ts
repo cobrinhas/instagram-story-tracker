074238d8e0a031aa521a43c986460cf239c355bc
freitzzz <1160907@isep.ipp.pt>
2024-01-12 13:23:02 +0000

    style: format

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,29 @@
-import { IgApiClient } from 'instagram-private-api'
+import { IgApiClient } from 'instagram-private-api';
 import { config as dotenv } from 'dotenv';
 
 type Config = {
-    username: string;
-    password: string;
-}
-
+	username: string;
+	password: string;
+};
 
 dotenv();
 const config = <Config>{ ...process.env };
 
-
 (async () => {
-    const ig = new IgApiClient();
+	const ig = new IgApiClient();
 
-    ig.state.generateDevice(config.username);
-    const auth = await ig.account.login(config.username, config.password);
+	ig.state.generateDevice(config.username);
+	const auth = await ig.account.login(config.username, config.password);
 
-    console.log(JSON.stringify(auth));
+	console.log(JSON.stringify(auth));
 
-    const targetUser = await ig.user.searchExact('ronaldo'); // getting exact user by login
-    const reelsFeed = ig.feed.reelsMedia({ // working with reels media feed (stories feed)
-        userIds: [targetUser.pk], // you can specify multiple user id's, "pk" param is user id
-    });
+	const targetUser = await ig.user.searchExact('ronaldo'); // getting exact user by login
+	const reelsFeed = ig.feed.reelsMedia({
+		// working with reels media feed (stories feed)
+		userIds: [targetUser.pk] // you can specify multiple user id's, "pk" param is user id
+	});
 
-    const storyItems = await reelsFeed.items(); // getting reels, see "account-followers.feed.example.ts" if you want to know how to work with feeds
+	const storyItems = await reelsFeed.items(); // getting reels, see "account-followers.feed.example.ts" if you want to know how to work with feeds
 
-    console.log(JSON.stringify(storyItems));
+	console.log(JSON.stringify(storyItems));
 })();
