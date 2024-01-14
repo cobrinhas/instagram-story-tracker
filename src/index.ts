34e11735ef99112d93a68f22389a533809654811
freitzzz <1160907@isep.ipp.pt>
2024-01-14 12:22:40 +0000

    feat: register vault dependencies in release mode

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,20 @@ import { Vault } from '@web-pacotes/vault';
 import { GlobalTracker } from './models';
 import {
 	FakeInstagramAuthenticationRepository,
-	FakeInstagramUserRepository
+	FakeInstagramUserRepository,
+	PrivateApiInstagramAuthenticationRepository,
+	PrivateApiInstagramUserRepository
 } from './data';
 import {
 	EmojiLumberdashClient,
 	putLumberdashToWork
 } from '@web-pacotes/lumberdash';
+import { IgApiClient } from 'instagram-private-api';
 
 putLumberdashToWork([new EmojiLumberdashClient()]);
 
 const config = load();
-const vault = createVault();
+const vault = createVault(false);
 
 const bot = initialize(config.bot_token, vault);
 
@@ -23,15 +26,29 @@ process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 bot.launch();
 
-function createVault(): Vault {
+function createVault(debugMode: boolean): Vault {
 	const vault = new Vault();
+	const client = new IgApiClient();
 
 	vault.store(<GlobalTracker>{}, 'GlobalTracker');
-	vault.store(
-		new FakeInstagramAuthenticationRepository(),
-		'InstagramAuthenticationRepository'
-	);
-	vault.store(new FakeInstagramUserRepository(), 'InstagramUserRepository');
+	vault.store(client, 'IgApiClient');
+
+	if (debugMode) {
+		vault.store(
+			new FakeInstagramAuthenticationRepository(),
+			'InstagramAuthenticationRepository'
+		);
+		vault.store(new FakeInstagramUserRepository(), 'InstagramUserRepository');
+	} else {
+		vault.store(
+			new PrivateApiInstagramAuthenticationRepository(client),
+			'InstagramAuthenticationRepository'
+		);
+		vault.store(
+			new PrivateApiInstagramUserRepository(client),
+			'InstagramUserRepository'
+		);
+	}
 
 	return vault;
 }
